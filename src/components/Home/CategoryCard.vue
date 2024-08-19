22c31abef00f41dd1e85c3ae3434b39547479a36
Mohamed Raafat <diab4085@gmail.com>
2024-08-19 21:06:53 +0300

    Update

--- src/components/Home/CategoryCard.vue
+++ src/components/Home/CategoryCard.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="card border border-zinc-200 font-poppins">
     <div class="py-4 px-5 flex justify-around items-center">
-      <div class="img">
-        <img src="/img/tool.png" alt="tool" />
-      </div>
       <div class="title">
-        <span class="text-[16x] text-black font-bold uppercase">{{ category }}</span>
+        <span class="text-[16x] text-black font-bold uppercase">{{ category.name }}</span>
       </div>
     </div>
   </div>
 </template>
 <script setup>
 defineProps({
-  category: String
+  category: Object
 })
 </script>
